c65b3b8417d1ede86cad60fc63c92cd0aeb4b40a
ealucas <ealucas@github.com>
2024-04-12 12:58:08 -0300

    to do: print_hex function

--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -1,18 +1,33 @@
 [org 0x7c00]
 
-	mov bx, HELLO_MSG
-	call print_string
-	mov bx, GOODBYE_MSG
-	call print_string
-	jmp $
+mov dx, 0x1fb6
+call print_hex
 
-	%include "src/print_string.asm"
+    jmp $
 
-	HELLO_MSG:
-		db "COLE ZE", 0
-	
-	GOODBYE_MSG:
-		db "TCHAU ZE", 0
+print_hex:
+    pusha
+    mov cx, 4
+hex_loop:
+    push cx
+    mov cx, 0x0f
+    and dx, cx
+    cmp dx, 0x09
+    jl hex_digit
+    add dx, 0x37
+hex_digit:
+    add dx, 0x30
+    mov ah, 0x0e
+    mov dl, dl
+    int 0x10
+    pop cx
+    shr dx, 4
+    loop hex_loop
+    popa
+    ret
+
+
+
 
 	times 510-($-$$) db 0
 	dw 0xaa55
